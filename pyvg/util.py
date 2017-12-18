3f0b7bb1705f5c6b36481d577466025d60b3bf64
Ivar Grytten <ivar.grytten@gmail.com>
2017-12-18 09:54:16 +0100

    Bugfix

--- pyvg/util.py
+++ pyvg/util.py
@@ -152,7 +152,7 @@ def vg_gam_file_to_intervals(vg_graph, vg_mapping_file_name,
         is_in = [mapping.start_position.node_id in offset_based_graph.blocks
                    for mapping in path.mappings]
         #assert all(is_in)
-        return is_in
+        return all(is_in)
     intervals = gam_file_to_intervals(vg_graph, vg_mapping_file_name,
                                       offset_based_graph,
                                       [is_in_graph,
