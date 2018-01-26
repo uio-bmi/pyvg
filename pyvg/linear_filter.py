eb28ae99be96cd28f9a732e31d4f29a13e803c3f
Ivar Grytten <ivar.grytten@gmail.com>
2018-01-26 15:39:27 +0100

    Changed linearfilter to behave on single graph

--- pyvg/linear_filter.py
+++ pyvg/linear_filter.py
@@ -1,4 +1,4 @@
-from util import get_paths_from_gam
+from util import vg_json_file_to_intervals
 import offsetbasedgraph as obg
 
 
@@ -7,45 +7,30 @@ def graph_to_indexed_interval(graph):
 
 
 class LinearFilter:
-    def __init__(self, position_tuples, indexed_intervals):
+    def __init__(self, position_tuples, indexed_interval):
         self._nodes = set()
-        for indexed_interval in indexed_intervals.values():
-            self._nodes.update(indexed_interval.region_paths)
-
+        self._nodes.update(indexed_interval.region_paths)
         self._position_tuples = position_tuples
-
-    def find_chrm(self, node):
-        for chrom, nodes in self._nodes.items():
-            if node in nodes:
-                return chrom, "+"
-            if -node in nodes:
-                return chrom, "-"
-        return None, None
+        self._indexed_interval = indexed_interval
 
     def find_start_positions(self):
-        dicts = {"+": {key: [] for key in self._nodes.keys()},
-                 "-": {key: [] for key in self._nodes.keys()}}
+        start_positions = {"+": [],
+                 "-": [] }
 
-        for pos in self._positions:
-            chrom, direction = self.find_chrm(position.region_path_id)
-            if chrom is None:
-                continue
-            indexed_interval = self._indexed_intervals[chrom]
-            dicts[direction][chrom] = indexed_interval.get_offset_at_position(
-                pos, direction)
-
-        chroms = (self.find_chrm(pos) for pos in self._position_tuples)
+        for pos in self._position_tuples:
+            direction = "+"
+            if pos.node_id < 0:
+                direction = "-"
+            start_positions[direction].append(self._indexed_interval.get_offset_at_position(
+                pos, direction))
+        return start_positions
 
     @classmethod
-    def from_gam_and_graphs(cls, gam_file_name, graphs):
-        paths = get_paths_from_gam(gam_file_name)
-        positions = (path.position for path in paths)
-        positions = (obg.Position(-position.node_id,
-                                  position.offset)
-                     if position.is_reverse else
-                     obg.Position(position.node_id,
-                                  position.offset)
-                     for position in positions)
-        indexed_intervals = [
-            graph_to_indexed_interval(graph) for graph in graphs]
-        return cls(positions, indexed_intervals)
+    def from_vg_json_reads_and_graph(cls, json_file_name, graph_file_name):
+        graph = obg.GraphWithReversals.from_unknown_file_format(graph_file_name)
+        intervals =  vg_json_file_to_intervals(None, json_file_name, graph)
+        positions = (interval.start_position for interval in intervals)
+
+        indexed_interval = graph.get_indexed_interval_through_graph()
+        return cls(positions, indexed_interval)
+
