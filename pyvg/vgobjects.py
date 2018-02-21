a49c77c3c9f22be99af4f80d921598e6799a2441
Ivar Grytten <ivar.grytten@gmail.com>
2018-02-21 15:41:56 +0100

    More cleanup

--- pyvg/vgobjects.py
+++ pyvg/vgobjects.py
@@ -258,21 +258,6 @@ class Alignment(object):
             alignment_dict["identity"])
 
 
-class AlignmentCollection(object):
-    def __init__(self, alignments):
-        self.alignments = alignments
-
-    @classmethod
-    def create_generator_from_file(cls, file_name):
-        f = open(file_name)
-        jsons = (json.loads(line) for line in f.readlines())
-        alignments = (Alignment.from_json(json_dict) for json_dict in jsons)
-        return cls(alignments)
-
-    def __iter__(self):
-        return self.alignments.__iter__()
-
-
 class Snarls(object):
     def __init__(self, snarls):
         self.snarls = snarls
