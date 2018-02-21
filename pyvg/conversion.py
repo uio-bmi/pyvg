a23ad28f0045501fd958f9376dc3b7f4ba96b516
Ivar Grytten <ivar.grytten@gmail.com>
2018-02-21 15:31:23 +0100

    Removed all GAM-related stuff

--- pyvg/conversion.py
+++ pyvg/conversion.py
@@ -1,73 +1,12 @@
 import json
 import logging
 from collections import defaultdict
-import offsetbasedgraph
+import offsetbasedgraph as obg
 from offsetbasedgraph import IntervalCollection
-
 from .sequences import SequenceRetriever
-from .vg import Graph, Alignment, Path, Mapping, Edit
+from .vgobjects import Graph, Alignment, Path, Mapping, Edit
 
 logger = logging.getLogger(__name__)
-
-
-def get_interval_for_sequence_in_ob_graph(start_node_id, reference_file_name,
-                                          ob_graph, vg_graph_file_name):
-    offset = 0
-    logger.info("Reading sequences")
-    reference_sequence = open(reference_file_name).read()
-    current_node = start_node_id
-
-    logger.info("Getting sequence retriever")
-    get_seq = SequenceRetriever.from_vg_graph(vg_graph_file_name).get_sequence_on_directed_node
-
-    while current_node is not None:
-        logger.debug("Current node: %d" % current_node)
-        node_sequence = get_seq(current_node)
-        current_ref_seq = reference_sequence[offset:offset + len(node_sequence)]
-        assert current_ref_seq == node_sequence
-
-        offset += len(node_sequence)
-        candidates = {}
-        max_length = 0
-        next_node = None
-        for next in ob_graph.adj_list[current_node]:
-            next_sequence = get_seq(next)
-
-            if next_sequence == reference_sequence[offset:offset+len(next_sequence)]:
-                candidates[next] = next_sequence
-                logger.debug(next_sequence)
-
-        assert len(candidates) <= 2
-        if len(candidates) > 1:
-            logger.debug(candidates)
-
-        max_length = 0
-        for node, seq in candidates.items():
-            if len(seq) > max_length:
-                next_node = node
-                max_length = len(seq)
-        current_node = next_node
-
-    logger.debug(offset)
-
-
-def get_chromosomes_from_vg_graph(vg_json_file_name):
-    # TODO
-    return ["chr3R", "chr3L", "chrX", "chrY", "chr4", "chr2R", "chr2L"]
-
-
-def vg_to_offsetbasedgraphs_per_chromosome(vg_json_file_name, to_file_base_name = "offset_based_graph_"):
-    """
-    Parse the vg graph chromosome by chromosome, create one
-    offsetbasedgraph per chromosome, and write to file.
-    :param vg_json_file_name:
-    :return:
-    """
-    for chromosome in get_chromosomes_from_vg_graph(vg_json_file_name):
-        logger.info("Creating graph for chromosome %s" % chromosome)
-        vg_graph = Graph.create_from_file(vg_json_file_name, False, chromosome)
-        offset_based_graph = vg_graph.get_offset_based_graph()
-        offset_based_graph.to_file(to_file_base_name + chromosome + ".tmp")
 
 
 def vg_mapping_file_to_interval_list(vg_graph, vg_mapping_file_name, offset_based_graph=False):
@@ -87,7 +26,6 @@ def vg_mapping_file_to_interval_list(vg_graph, vg_mapping_file_name, offset_base
             logger.info("Alignments processed: %d" % i)
         i += 1
 
-        #alignments.append(Alignment.from_json(json_dict))
         alignment = Alignment.from_json(json_dict)
         path = alignment.path
         if vg_graph:
@@ -114,22 +52,6 @@ def path_is_reverse(path):
 
     return False
 
-
-def get_paths_from_gam(filename):
-    import stream
-    from pyvg import vg_pb2
-    return (alignment.path for alignment in
-            stream.parse(filename,
-                         vg_pb2.Alignment)
-            if alignment.identity == 1.0)
-
-#@profile
-def get_json_paths_from_gam(filename):
-    with open(filename) as f:
-        out = (json.loads(line)["path"] for line in f.readlines())
-
-    return out
-
 def jsonpath_to_path(json_path):
     return Path.from_json(json_path)
 
@@ -142,18 +64,13 @@ def protopath_to_path(proto_path):
     return Path(proto_path.name, mappings)
 
 
-def gam_file_to_intervals(vg_graph, mapping_file_name,
-                          ob_graph, filter_funcs=()):
-    logging.info("Getting alignments from: %s" % mapping_file_name)
-    proto_paths = get_paths_from_gam(mapping_file_name)
-    paths = (protopath_to_path(proto_path) for proto_path in proto_paths)
-    paths = (path for path in paths if
-             all(filter_func(path) for filter_func in filter_funcs))
+def get_json_paths_from_gam(filename):
+    with open(filename) as f:
+        out = (json.loads(line)["path"] for line in f.readlines())
 
-    intervals = (path.to_obg_with_reversals(ob_graph) for path in paths)
-    return (i for i in intervals if i is not False)
+    return out
 
-#@profile
+
 def vg_json_file_to_intervals(vg_graph, mapping_file_name,
                           ob_graph=None, filter_funcs=()):
     logging.info("Getting json reads from: %s" % mapping_file_name)
@@ -167,59 +84,11 @@ def vg_json_file_to_intervals(vg_graph, mapping_file_name,
 
 
 def vg_json_file_to_interval_collection(vg_graph, vg_mapping_file_name, offset_based_graph=None):
-    return offsetbasedgraph.IntervalCollection(
+    return obg.IntervalCollection(
                 vg_json_file_to_intervals(
                     vg_graph, vg_mapping_file_name,
                     offset_based_graph)
             )
-
-
-def vg_gam_file_to_intervals(vg_graph, vg_mapping_file_name,
-                             offset_based_graph=False,
-                             max_intervals=False):
-
-    def is_in_graph(path):
-        #assert isinstance(offset_based_graph, offsetbasedgraph.GraphWithReversals)
-        #assert all(isinstance(mapping.start_position.node_id, int) for mapping in path.mappings)
-        is_in = (mapping.start_position.node_id in offset_based_graph.blocks
-                 for mapping in path.mappings)
-        #assert all(is_in)
-        return all(is_in)
-    intervals = gam_file_to_intervals(vg_graph, vg_mapping_file_name,
-                                      offset_based_graph,
-                                      [is_in_graph])
-                                       #lambda path: path.is_identity()])
-                                       #lambda path: np.random.randint(0, 6) == 1]
-
-    return intervals
-
-
-def vg_gam_file_to_interval_collection(
-        vg_graph, vg_mapping_file_name,
-        offset_based_graph=False, max_intervals=False):
-    return offsetbasedgraph.IntervalCollection(
-                vg_gam_file_to_intervals(
-                    vg_graph, vg_mapping_file_name,
-                    offset_based_graph=offset_based_graph,
-                    max_intervals=max_intervals)
-            )
-
-
-def vg_gam_file_to_interval_list(vg_graph, vg_mapping_file_name,
-                                 offset_based_graph=False,
-                                 max_intervals=False):
-    intervals = []
-    collection = offsetbasedgraph.IntervalCollection(
-                vg_gam_file_to_intervals(
-                    vg_graph, vg_mapping_file_name,
-                    offset_based_graph=offset_based_graph,
-                    max_intervals=max_intervals)
-            )
-    for interval in collection:
-        intervals.append(interval)
-
-    return intervals
-
 
 def vg_path_to_obg_interval(path, ob_graph=False):
     mappings = []
@@ -230,41 +99,6 @@ def vg_path_to_obg_interval(path, ob_graph=False):
     path = Path(path.name, mappings)
     interval = path.to_obg_with_reversals(ob_graph)
     return interval
-
-    if len(path.mapping) == 0:
-        return False
-
-    nodes = [mapping.position.node_id for mapping in path.mapping]
-
-    if path_is_reverse(path):
-        if not ob_graph:
-            raise Exception("Path is reverse and offset based graph is not sent")
-
-        nodes = nodes[::-1]
-        start_block_length = ob_graph.blocks[nodes[0]].length()
-        start_offset = start_block_length - mapping_end_offset(
-            path.mapping[-1])
-        end_block_length = ob_graph.blocks[nodes[-1]].length()
-        end_offset = end_block_length - mapping_end_offset(path.mapping[0])
-        direction = -1
-    else:
-        start_offset = path.mapping[0].position.offset
-        end_offset = mapping_end_offset(path.mapping[-1])
-        direction = 1
-
-    interval_graph = None
-    if ob_graph:
-        interval_graph = ob_graph
-    interval = offsetbasedgraph.Interval(
-        start_offset, end_offset,
-        nodes, interval_graph, direction=direction)
-    if end_offset == 0 or interval.end_offset == 0:
-        print("#", path)
-        print(interval)
-        raise Exception("Endoffset == 0")
-    print(interval)
-    return interval
-
 
 def vg_mapping_file_to_interval_file(out_file_name, vg_graph,
                                      vg_mapping_file_name,
@@ -335,14 +169,43 @@ def json_file_to_obg_numpy_graph(json_file_name, n_nodes = 0):
     graph.blocks.node_id_offset = min_node_id - 1
     return graph
 
-def vg_gam_file_to_gzip_interval_file(out_file_name, vg_graph,
-                                      vg_mapping_file_name,
-                                      offset_based_graph=False):
-    interval_collection = IntervalCollection(
-                vg_gam_file_to_intervals(vg_graph, vg_mapping_file_name,
-                                         offset_based_graph))
-    interval_collection.to_gzip(out_file_name)
-    return out_file_name
 
-if __name__ == "__main__":
-    vg_to_offsetbasedgraphs_per_chromosome("../tests/x.json", "test_ofbg_")
+def get_interval_for_sequence_in_ob_graph(start_node_id, reference_file_name,
+                                          ob_graph, vg_graph_file_name):
+    offset = 0
+    logger.info("Reading sequences")
+    reference_sequence = open(reference_file_name).read()
+    current_node = start_node_id
+
+    logger.info("Getting sequence retriever")
+    get_seq = SequenceRetriever.from_vg_graph(vg_graph_file_name).get_sequence_on_directed_node
+
+    while current_node is not None:
+        logger.debug("Current node: %d" % current_node)
+        node_sequence = get_seq(current_node)
+        current_ref_seq = reference_sequence[offset:offset + len(node_sequence)]
+        assert current_ref_seq == node_sequence
+
+        offset += len(node_sequence)
+        candidates = {}
+        max_length = 0
+        next_node = None
+        for next in ob_graph.adj_list[current_node]:
+            next_sequence = get_seq(next)
+
+            if next_sequence == reference_sequence[offset:offset+len(next_sequence)]:
+                candidates[next] = next_sequence
+                logger.debug(next_sequence)
+
+        assert len(candidates) <= 2
+        if len(candidates) > 1:
+            logger.debug(candidates)
+
+        max_length = 0
+        for node, seq in candidates.items():
+            if len(seq) > max_length:
+                next_node = node
+                max_length = len(seq)
+        current_node = next_node
+
+    logger.debug(offset)
