c66f88b97fd32eb491898479b5498b0894c36d6d
Ivar Grytten <ivar.grytten@gmail.com>
2018-06-08 13:37:55 +0200

    Bugfix

--- pyvg/view.py
+++ pyvg/view.py
@@ -3,6 +3,6 @@ from .util import call_vg
 
 
 def graph_to_json(graph_file_name, json_file_name):
-    call_vg("vg view -Vj %s > %s" % (graph_file_name, json_file_name))
+    call_vg("vg view -Vj %s" % graph_file_name, out_file=json_file_name)
     logging.info("Done converting to json. Wrote to %s" % json_file_name)
 
