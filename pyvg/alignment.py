e6b184dd6791b577a029025f46c84f1f217229c8
Knut Rand <knutdrand@gmail.com>
2018-06-11 22:49:58 +0200

    chunking alignments

--- pyvg/alignment.py
+++ pyvg/alignment.py
@@ -1,4 +1,5 @@
 import re
+from itertools import islice, chain
 from .util import call_vg
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 from subprocess import Popen, PIPE
@@ -27,7 +28,7 @@ def align_sequence(sequence_id, sequence, vg_graph_file_name):
     with open("tmp.vg", "wb") as tmp_file:
         tmp_file.write(vg_alignment)
 
-    json_alignment = call_vg("vg view -aj tmp.vg")
+    json_alignment = call_vg("vg view -aj 80tmp.vg")
     return json_alignment
 
 
@@ -47,6 +48,11 @@ def align_fasta_to_graph(fasta_file_name, graph_file_name, out_file_name):
     logging.info("Wrote to %s" % out_file_name)
 
 
+def _chunks(iterator, N):
+    for first in iterator:
+        yield chain([first], islice(iterator, N-1))
+
+
 def align_fasta_to_graph_paralell(fasta_file_name, graph_file_name, out_file_name):
     output_file = open(out_file_name, "w")
 
@@ -55,12 +61,12 @@ def align_fasta_to_graph_paralell(fasta_file_name, graph_file_name, out_file_nam
                                json_alignment, re.IGNORECASE).group(1)
         assert found_name == name, (name, found_name)
         return json_alignment
-
-    pipes = {title: align_safe(title.split()[0], sequence, graph_file_name)
-             for title, sequence in fasta_parser(fasta_file_name)}
-    alignments = (assert_name(title, pipe.read())
-                  for title, pipe in pipes.items())
-
-    with open(out_file_name, "w") as output_file:
+    output_file = open(out_file_name, "w")
+    for chunk in _chunks(fasta_parser(fasta_file_name), 100):
+        pipes = {title: align_safe(title.split()[0], sequence, graph_file_name)
+                 for title, sequence in chunk}
+        alignments = (assert_name(title, pipe.read())
+                      for title, pipe in pipes.items())
         output_file.writelines(alignments)
+    output_file.close()
     logging.info("Wrote to %s" % out_file_name)
