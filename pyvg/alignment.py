299797ca7a49969d455adb351a4e6b4c857256b7
Knut Rand <knutdrand@gmail.com>
2018-06-11 21:52:10 +0200

    paralell alignment

--- pyvg/alignment.py
+++ pyvg/alignment.py
@@ -10,12 +10,16 @@ def fasta_parser(file_name):
         for title, sequence in SimpleFastaParser(f):
             yield title, sequence
 
+
 def align_safe(sequence_id, sequence, vg_graph_file_name):
-    command = "vg align -Q %s -s %s %s" % (sequence_id, sequence, vg_graph_file_name)
+    command = "vg align -Q %s -s %s %s" % (sequence_id, sequence,
+                                           vg_graph_file_name)
     align_process = Popen(command.split(), stdout=PIPE, stdin=PIPE)
     command = "vg view -aj -"
-    view_process = Popen(command.split(), stdin=align_process.stdout, stdout=PIPE)
+    view_process = Popen(command.split(), stdin=align_process.stdout,
+                         stdout=PIPE)
     return view_process.stdout
+
 
 def align_sequence(sequence_id, sequence, vg_graph_file_name):
     vg_alignment = call_vg("vg align -Q %s -s %s %s" % (sequence_id, sequence, vg_graph_file_name),
@@ -25,6 +29,7 @@ def align_sequence(sequence_id, sequence, vg_graph_file_name):
 
     json_alignment = call_vg("vg view -aj tmp.vg")
     return json_alignment
+
 
 def align_fasta_to_graph(fasta_file_name, graph_file_name, out_file_name):
     output_file = open(out_file_name, "w")
@@ -36,29 +41,26 @@ def align_fasta_to_graph(fasta_file_name, graph_file_name, out_file_name):
             name = name_search.group(1)
         else:
             raise Exception("Did not found name")
-        
         print("  Matched %s" % name)
         output_file.writelines([json_alignment])
     output_file.close()
     logging.info("Wrote to %s" % out_file_name)
 
+
 def align_fasta_to_graph_paralell(fasta_file_name, graph_file_name, out_file_name):
     output_file = open(out_file_name, "w")
-    name_search = lambda x: re.search(r"\"name\":\"([A-Z0-9_\-\.a-z]+)+\"", x, re.IGNORECASE)
-    alignment_pipes = {title: align_safe(title.split()[0], sequence, graph_file_name)
-                       for title, name in fasta_parser(fasta_file_name)}
-    names = {title: pipe.read() for title, pipe in alignment_pipes()}
 
-    for title, sequence in fasta_parser(fasta_file_name):
-        print("Aligning %s" % title)
-        json_alignment = align_sequence("%s" % title.split()[0], sequence, graph_file_name)
-        name_search = re.search(r"\"name\":\"([A-Z0-9_\-\.a-z]+)+\"", json_alignment, re.IGNORECASE)
-        if name_search:
-            name = name_search.group(1)
-        else:
-            raise Exception("Did not found name")
-        
-        print("  Matched %s" % name)
-        output_file.writelines([json_alignment])
-    output_file.close()
+    def assert_name(name, json_alignment):
+        found_name = re.search(r"\"name\":\"([A-Z0-9_\-\.a-z]+)+\"",
+                               json_alignment, re.IGNORECASE).group(1)
+        assert found_name == name, (name, found_name)
+        return json_alignment
+
+    pipes = {title: align_safe(title.split()[0], sequence, graph_file_name)
+             for title, sequence in fasta_parser(fasta_file_name)}
+    alignments = (assert_name(title, pipe.read())
+                  for title, pipe in pipes.items())
+
+    with open(out_file_name, "w") as output_file:
+        output_file.writelines(alignments)
     logging.info("Wrote to %s" % out_file_name)
