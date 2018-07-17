1136c5da7a4e1950bbb3b0730c644cf1081ca4ce
root <ivarknut@server>
2018-07-17 14:50:47 +0200

    Rewrite aligment collection to store intervals

--- pyvg/alignmentcollection.py
+++ pyvg/alignmentcollection.py
@@ -2,20 +2,22 @@ import numpy as np
 from .conversion import parse_vg_json_alignments
 from collections import defaultdict
 import pickle
+from offsetbasedgraph import IntervalCollection
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 class AlignmentCollection:
 
-    def __init__(self, node_dict, graph):
+    def __init__(self, node_dict, graph, intervals):
         self._node_dict = node_dict
         self._graph = graph
+        self.intervals = intervals
 
     @classmethod
     def from_vg_json_file(cls, file_name, ob_graph):
         alignments = parse_vg_json_alignments(file_name, ob_graph)
-
+        intervals = []
         node_dict = defaultdict(set)
         i = 0
         for name, interval in alignments:
@@ -23,27 +25,32 @@ class AlignmentCollection:
                 logging.info("#%d processed" % i)
 
             for rp in interval.region_paths:
-                node_dict[rp].add(name)
+                node_dict[rp].add((name, i))
+            intervals.append(interval)
             i += 1
+        
 
-        return cls(node_dict, ob_graph)
+        return cls(node_dict, ob_graph, intervals)
 
     def to_file(self, file_name):
         logging.info("Writing to file")
         with open(file_name, "wb") as f:
             pickle.dump(self._node_dict, f)
 
+        IntervalCollection(self.intervals).to_file(file_name + ".intervals")
+        
     @classmethod
     def from_file(cls, file_name, graph):
         logging.info("Reading from file")
         with open(file_name, "rb") as f:
             node_dict = pickle.load(f)
-            return cls(node_dict, graph)
 
+        intervals = IntervalCollection.from_file(file_name + ".intervals", graph=graph)
+        return cls(node_dict, graph, list(intervals))
 
     def get_alignments_on_node(self, node_id):
-        return self._node_dict[node_id]
-
+        name, index = self._node_dict[node_id]
+        return (name, self.intervals[index])
 
     def get_alignments_on_interval(self, interval):
         alignments = set()
